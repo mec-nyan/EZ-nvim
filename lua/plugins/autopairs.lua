6abbfbccba5a3be2534a062e92d77359b6c6565a
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-07-12 19:32:53 -0300

    Add autotag support.

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,16 @@
 return {
-	'windwp/nvim-autopairs',
-	event = "InsertEnter",
-	opts = {
-		enable_bracket_in_quote = false,
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		opts = {
+			enable_bracket_in_quote = false,
+		},
 	},
+	{
+		'windwp/nvim-ts-autotag',
+		lazy = false,
+		config = function ()
+			require"nvim-ts-autotag".setup()
+		end
+	}
 }
