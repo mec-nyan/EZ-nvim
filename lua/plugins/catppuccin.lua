138529ef129e52b0a4d985f19df9537666602e52
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-08-30 15:27:33 -0300

    Colorscheme: Make some bgs darker (Pmenu, Nvimtree).

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,6 +6,10 @@ return {
 		require "catppuccin".setup {
 			flavour = "mocha",
 			transparent_background = true,
+			float = {
+				transparent = true,
+				solid = true,
+			},
 			show_end_of_buffer = false,
 			dim_inactive = {
 				enabled = false,
@@ -13,6 +17,23 @@ return {
 				percentage = 2,
 			},
 			no_bold = true,
+			styles = {
+				comments = { "italic" },
+			},
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				nvimtree = true,
+				dap = true,
+				dap_ui = true,
+				gitsigns = true,
+			},
+			custom_highlights = function(colors)
+				return {
+					Pmenu = { bg = colors.mantle },
+					NvimTreeNormal = { bg = colors.mantle },
+				}
+			end
 		}
 
 		vim.cmd.colorscheme "catppuccin"
