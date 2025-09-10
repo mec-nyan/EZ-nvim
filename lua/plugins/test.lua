e3fc3ac2bcc602a18debbd08eac8478bfd4007b5
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-09-10 20:32:21 -0300

    Add config for neotest.

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,9 +1,20 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
-  }
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-python",
+		-- "nvim-neotest/neotest-go",
+		"fredrikaverpil/neotest-golang"
+	},
+	config = function()
+		require "neotest".setup {
+			adapters = {
+				require "neotest-python",
+				require "neotest-golang"
+			}
+		}
+	end
 }
